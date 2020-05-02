50590ed828e36a698011d34826454c4bac93e7cf
Wojciech Mosieński <w.mosienski@gmail.com>
2020-05-02 23:32:21 +0200

    zad1

--- src/main/java/myProject/App.java
+++ src/main/java/myProject/App.java
@@ -8,6 +8,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Boolean isUnder18Var;
+        isUnder18Var = isUnderAge(7, true);
+        System.out.println(isUnder18Var);
+    }
+
+    public static Boolean isUnderAge(int age, Boolean isFromWisconsin) {
+        if (isFromWisconsin) {
+            // sth here
+        }
+        if (age < 21) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
