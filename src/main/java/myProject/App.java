025c687fd76b53776a47978a3b95030e3d3e2477
Wojciech Mosieński <w.mosienski@gmail.com>
2020-05-03 01:32:56 +0200

    asd

--- src/main/java/myProject/App.java
+++ src/main/java/myProject/App.java
@@ -8,16 +8,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        Boolean isUnder18Var;
-        isUnder18Var = isUnderAge(19, true);
-        System.out.println(isUnder18Var);
-        isUnder18Var = isUnderAge(17, true);
-        System.out.println(isUnder18Var);
-        isUnder18Var = isUnderAge(30, false);
-        System.out.println(isUnder18Var);
-        isUnder18Var = isUnderAge(19, false);
-        System.out.println(isUnder18Var);
+        int[] myArray = doSth();
+        System.out.println(myArray[0]);
+        System.out.println(myArray[1]);
+        System.out.println(myArray[2]);
+        System.out.println(myArray[3]);
+        System.out.println(myArray[4]);
+        System.out.println(myArray[5]);
+        System.out.println(myArray[6]);
+        System.out.println(myArray[7]);
+        System.out.println(myArray[8]);
+        System.out.println(myArray[9]);
+    }
 
+    public static int[] doSth() {
+        int[] myArray = new int[10];
+        for (int i = 0; i < 10; i++) {
+            myArray[i] = i*2;
+        }
+        return myArray;
     }
 
     public static Boolean isUnderAge(int age, Boolean isFromWisconsin) {
